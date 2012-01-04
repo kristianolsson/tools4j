04c77053b196d93069a0f041a5295b7dfa5ba3c4
Kristoffer Sjögren <stoffe@office.deephacks.org>
2012-01-05 00:21:59 +0100

    small rephrase

--- config/config-model-api/src/main/java/org/deephacks/tools4j/config/model/package-info.java
+++ config/config-model-api/src/main/java/org/deephacks/tools4j/config/model/package-info.java
@@ -1,7 +1,7 @@
 /**
-* This package contains simple canonical data structures which only purpose is 
-* to convey request and response information between different services in 
-* the architecture. The intent is to decouple services from each other.
+* Contains simple canonical data structures which only purpose is to convey request 
+* and response information between different services in  the architecture. The 
+* intent is to decouple services from each other.
 */
 package org.deephacks.tools4j.config.model;
 
