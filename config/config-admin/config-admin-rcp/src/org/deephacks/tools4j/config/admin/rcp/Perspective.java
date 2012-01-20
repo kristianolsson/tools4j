d8c0e16df8f536526882ce837b12d8456330e8b7
Kristoffer Sjögren <kristoffer.sjogren@aphelion.se>
2012-01-20 18:02:05 +0100

    added eclipse swt rcp for admin, this is not working yet

---
+++ config/config-admin/config-admin-rcp/src/org/deephacks/tools4j/config/admin/rcp/Perspective.java
@@ -0,0 +1,10 @@
+package org.deephacks.tools4j.config.admin.rcp;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
